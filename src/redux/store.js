e938068b1958b5ae3289d3c23a8fef3540c0835c
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-05 17:08:13 +0100

    edited store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -18,13 +18,11 @@ const reducers = combineReducers({
 
 const store = createStore(
     reducers,
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-      : compose
-    )
-  );
+     initialState,
+      compose(
+          applyMiddleware(...middleware),
+           window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+);
 
 export default store;
