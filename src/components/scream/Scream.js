a2ab09feff2e186b7676bbf5c6718e08dac396be
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-05 13:10:20 +0100

    corrected a dependancy on scream.js

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -15,7 +15,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 //MUI stuff
-import Card from '@material-ui/core/card';
+import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
